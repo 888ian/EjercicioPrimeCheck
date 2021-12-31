53f987dbdd983f82250ec00165afc8dfc77aeddc
Ian Wohlgemuth <ianwohlgemuth@gmail.com>
2021-12-31 00:28:37 -0300

    segundoCommit

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-/*Un número primo es aquel que solo es divisible por 1 y sí mismo. A su vez, un número
+/*
+Un número primo es aquel que solo es divisible por 1 y sí mismo. A su vez, un número
 es primo si no es divisible por los números primos que lo anteceden hasta su raíz
 cuadrada. Ejemplo, para saber si 9833 es primo debería ser dividido al menos por
 todos los números primos anteriores hasta su raíz cuadrada, en este caso 99. Si no es
-divisible por ninguno de ellos entonces podemos decir que es primo.*/
-//Se requiere una interfaz que permita saber si un número es o no primo
-
+divisible por ninguno de ellos entonces podemos decir que es primo.
+Se requiere una interfaz que permita saber si un número es o no primo
+*/
 
 //main
 
 function esPrimo(num) {
   //El 1,0 o números negativos hardcoded
   if (num <= 1) {
-      return false
+    return false;
   }
 
   //Declaro sqrt como la raíz del número
@@ -27,7 +28,10 @@ function esPrimo(num) {
   return true;
 }
 
-//testeo
+var butt = document.getElementById("button");
+butt.onclick()
+
+/*testeo
 
 console.log(esPrimo(1), " Esto debería ser false");
 console.log(esPrimo(2), " Esto debería ser true");
@@ -40,3 +44,4 @@ primes.forEach(element => {
     }
 });
 console.log("test finished");
+*/
